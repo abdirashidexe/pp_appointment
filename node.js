7bb2f6c2a03fe97713451449000834830fa2cf65
rjfredr <fredrickalexander1727@gmail.com>
2025-01-30 14:31:25 -0800

    rj add

--- node.js
+++ node.js
@@ -8,6 +8,13 @@ app.use(express.urlencoded({extended: true}));
 
 const PORT = 3000;
 
+const orderDetails = [];
+
 app.listen(PORT, (req,res) => {
     console.log(`Server is running at http://localhost:${PORT}`);
+})
+
+app.post('/thankyou', (req,res) => {
+    orderDetails.push(req.body);
+    res.sendFile(`${import.meta.dirname}/thankyou.html`);
 })
