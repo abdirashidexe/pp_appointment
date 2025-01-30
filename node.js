5c5926c691399fe54c99281756f326ce2c578907
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-01-30 14:36:23 -0800

    node update

--- node.js
+++ node.js
@@ -12,9 +12,14 @@ const orderDetails = [];
 
 app.listen(PORT, (req,res) => {
     console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/home.html`);
 })
 
 app.post('/thankyou', (req,res) => {
     orderDetails.push(req.body);
     res.sendFile(`${import.meta.dirname}/thankyou.html`);
-})
+    console.log(req.body);
+});
